46c50db8742cb601f529140e0cedc2d9f5639ea8
Darren Semusemu <darrensemusemu@gmail.com>
2024-10-16 12:26:44 +0200

    natskv: enocode all keys as base32 strings

--- index/upsidedown/store/natskv/key.go
+++ index/upsidedown/store/natskv/key.go
@@ -1,17 +1,19 @@
 package natskv
 
-import "encoding/base64"
+import (
+	"encoding/base32"
+)
 
 type EncondedKey struct {
 	b []byte
 }
 
 func (k *EncondedKey) encode() string {
-	return base64.StdEncoding.EncodeToString(k.b)
+	return base32.StdEncoding.EncodeToString(k.b)
 }
 
 func (k *EncondedKey) decode(key string) ([]byte, error) {
 	var err error
-	k.b, err = base64.StdEncoding.DecodeString(key)
+	k.b, err = base32.StdEncoding.DecodeString(key)
 	return k.b, err
 }
